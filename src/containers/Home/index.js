65ce4997aaad7ea974f4267ed602645f6a4f02c7
Ben Janes <brjanes@gmail.com>
2016-04-28 22:43:50 -0400

    Add project scaffolding. Closes #13 #14 #4 (#20)
    * Add project scaffolding
    
    * Copy over some important details from project boilerplate readme

---
+++ src/containers/Home/index.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import DocumentMeta from 'react-document-meta';
+
+/* components */
+import { DataDisplay } from 'components/DataDisplay';
+
+const metaData = {
+  title: 'Twitch Chat Visualizer',
+  description: 'Get a sense of how a Twitch channel is devolving',
+  canonical: 'http://example.com/path/to/page',
+  meta: {
+    charset: 'utf-8',
+    name: {
+      keywords: 'twitch,chat,sentiment',
+    },
+  },
+};
+
+export class Home extends Component {
+  render() {
+    return (
+      <section>
+        <DocumentMeta {...metaData} />
+        <DataDisplay />
+      </section>
+    );
+  }
+}
