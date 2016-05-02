4ee4667ed1ada330d2c7f3f09e180e142aeba4ed
Terry Capan <tcapanjr@gmail.com>
2016-05-02 17:12:24 -0500

    add chart component

---
+++ src/components/Chart/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Sparklines, SparklinesLine, SparklinesSpots } from 'react-sparklines';
+import _ from 'lodash';
+
+export default (props) => {
+
+  return (
+    <div>
+      <Sparklines data={props.data} limit={20}>
+        <SparklinesLine color={props.color} />
+        <SparklinesSpots />
+      </Sparklines>
+      <div>{Math.round(props.data)}</div>
+    </div>
+  );
+};
