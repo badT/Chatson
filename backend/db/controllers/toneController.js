3ef6a32d3dc2f1d0bb04f7c938976bf7a9b76052
Dan Ramich <dramich@users.noreply.github.com>
2016-05-03 14:51:34 -0700

    Update tone schema, move tone listener (#45)
    * Add listener for messages and db tone update
    
    * Move tone listener to own file
    
    * Update tone schema, move listener to controlle file

--- backend/db/controllers/toneController.js
+++ backend/db/controllers/toneController.js
@@ -1,12 +1,26 @@
-const path = require('path');
 const Tone = require('../schemas/toneSchema');
 
 
 exports.saveTone = (tone) => {
   const newTone = new Tone(tone);
   newTone.save().then((result) => {
-    console.log('Tone saved:', result);
+    // console.log('Tone saved:', result);
+    return result;
   }).error((err) => {
     console.log('toneController error:', err);
   });
 };
+
+Tone.changes().then((feed) => {
+  feed.each((error, doc) => {
+    if (error) {
+      console.log(error);
+      process.exit(1);
+    } else {
+      console.log('changes feed: ', doc);
+    }
+  });
+}).error((error) => {
+  console.log(error);
+  process.exit(1);
+});
