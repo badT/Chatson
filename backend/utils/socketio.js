cf8c2f3d1bff088d662befeee7f917e5443b0e45
Dan Ramich <danold215@gmail.com>
2016-05-02 17:03:45 -0700

    Add listener for messages and db tone update

--- backend/utils/socketio.js
+++ backend/utils/socketio.js
@@ -2,7 +2,10 @@ const twitchIO = require('socket.io');
 const twitchClient = require('./tmiClient');
 const Rx = require('rx');
 const eventEmitter = require('./eventEmitter');
-const messageController = require('../db/messageController');
+// const messageController = require('../db/controllers/messageController');
+const Tone = require('../db/schemas/toneSchema');
+const analyzer = require('../watson/analyzer');
+
 
 const messageSource = Rx.Observable.fromEvent(twitchClient, 'chat', (channel, user, msg) => {
   return { channel, user, msg };
@@ -10,7 +13,26 @@ const messageSource = Rx.Observable.fromEvent(twitchClient, 'chat', (channel, us
 
 const messageSubscription = messageSource.subscribe((msgObj) => {
   eventEmitter.emit('chatMessage', msgObj);
-  // messageController.saveMessage(msgObj);
+});
+
+// Listenes for a new chat message and moves the message into the analyzer file
+eventEmitter.on('chatMessage', (msgObj) => {
+  analyzer.intoTones(msgObj);
+});
+
+// Listens for chanes to Tone table and console logs it
+Tone.changes().then((feed) => {
+  feed.each((error, doc) => {
+    if (error) {
+      console.log(error);
+      process.exit(1);
+    } else {
+      console.log('changes feed: ', doc);
+    }
+  });
+}).error((error) => {
+  console.log(error);
+  process.exit(1);
 });
 
 module.exports = server => {
