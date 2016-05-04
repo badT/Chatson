423a9dd74a2b6172d36345c5af1c2eb92b877dc4
Terry Capan <tcapanjr@gmail.com>
2016-05-04 18:12:07 -0500

    Add react-d3-basic

--- src/components/LineGraph/index.js
+++ src/components/LineGraph/index.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { LineChart } from 'react-d3-basic';
 
 export default (props) => {
-  return (
+  const width = 700;
+  const height = 300;
+  const margins = {left: 100, right: 100, top: 50, bottom: 50};
+  const chartSeries = [
+    {},
+    {},
+    {},
+    {},
+    {},
+  ];
+  // x accesor function?
+  const x = function() {};
 
-  );
+    return (
+      <LineChart
+
+      />
+    );
 };
