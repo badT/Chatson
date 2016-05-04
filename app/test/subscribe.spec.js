be9b5da579c49775d6df5d15e01e5b92c9f4a176
Dan Ramich <danold215@gmail.com>
2016-05-04 15:14:13 -0700

    Add base for testing, update package.json

---
+++ app/test/subscribe.spec.js
@@ -0,0 +1,14 @@
+const expect = require('expect');
+const tone = require('../db/controllers/toneController.js');
+
+// add = (a, b) => { return a + b; };
+
+describe('test suite', function () {
+  it('should expose a function', function () {
+    expect(tonefunction).toExist('function');
+  });
+
+  // it('should do math', function () {
+  //   expect(add(1, 3)).to.equal(4);
+  // });
+});
