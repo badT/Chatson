cd32629235ce946648db7db166a43cfcf10ef86f
Ben Janes <brjanes@gmail.com>
2016-05-04 17:33:06 -0400

    Add a container to the DataDisplay component to re-mount components on selected channel change

--- src/components/DataDisplay/index.js
+++ src/components/DataDisplay/index.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 
-import MessageDisplay from '../../containers/Message/message';
-import ChannelData from '../../containers/ChannelData/channelData';
-import EmotionDisplay from '../../containers/WatsonEmotion/emotionData';
-import SocialDisplay from '../../containers/WatsonSocial/socialData';
+import DataContainer from '../../containers/DataContainer/DataContainer';
 
 /* component styles */
 import { styles } from './styles.scss';
@@ -12,44 +9,7 @@ export class DataDisplay extends Component {
   render() {
     return (
       <div>
-        <MessageDisplay socket={this.props.socket} />
-        <section>
-          <div className="row">
-            <div className="col-md-6">
-              <table className="table table-hover">
-                <thead>
-                  <tr>
-                    <th>Emotional Attributes</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>
-                      <EmotionDisplay socket={this.props.socket} />
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-            <div className="col-md-6">
-              <table className="table table-hover">
-                <thead>
-                  <tr>
-                    <th>Social Attributes</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>
-                      <SocialDisplay />
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </section>
-        <ChannelData socket={this.props.socket} />
+        <DataContainer socket={this.props.socket}/>
       </div>
     );
   }
