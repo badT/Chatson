7fed60ee477000878f509342afb77a68cff6acaa
Ben Janes <brjanes@gmail.com>
2016-05-04 11:13:41 -0400

    Add styles to dropdown component

--- src/containers/ChannelDropdown/ChannelDropdown.js
+++ src/containers/ChannelDropdown/ChannelDropdown.js
@@ -3,6 +3,9 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { getChannels, setChannel } from '../../actions/index';
 
+/* component styles */
+import { styles } from './styles.scss';
+
 class ChannelDropdown extends Component {
   componentWillMount() {
     this.props.getChannels();
@@ -11,19 +14,21 @@ class ChannelDropdown extends Component {
   renderChannels(channelData) {
     const name = channelData.channel.name;
     return (
-      <li key={name} onClick={() => this.props.setChannel(name)}>{name}</li>
+      <li key={name} className="dropdown-menu-item" onClick={() => this.props.setChannel(name)}>{name}</li>
     );
   }
 
   render() {
     return (
-      <div className="btn-group">
-        <button type="button" className="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Top Channels <span className="caret"></span>
-        </button>
-        <ul className="dropdown-menu">
-          {this.props.channels.list.map(this.renderChannels, this)}
-        </ul>
+      <div className={`${styles}`}>
+        <div className="btn-group">
+          <button type="button" className="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Top Channels <span className="caret"></span>
+          </button>
+          <ul className="dropdown-menu">
+            {this.props.channels.list.map(this.renderChannels, this)}
+          </ul>
+        </div>
       </div>
     );
   }
