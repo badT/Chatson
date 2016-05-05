b4d281dd05b357c63edbb1610037bd6b455bc96d
Andrew Vedady <drewvedady@gmail.com>
2016-05-05 09:03:19 -0400

    Add de Bar Chart

---
+++ src/components/BarGraph/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BarChart } from 'react-d3-basic';
+
+export default (props) => {
+  return (
+    <BarChart
+      title={props.title}
+      data={props.data}
+      width={props.width}
+      height={props.height}
+      margins={props.margins}
+      chartSeries={props.chartSeries}
+      x={props.x}
+      xScale={props.xScale}
+      y={props.y}
+      yTicks={props.yTicks} />
+  );
+};
+
