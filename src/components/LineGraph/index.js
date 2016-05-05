ae90deb09de4810949de9d79c162c99bd897d7f2
Terry Capan <tcapanjr@gmail.com>
2016-05-05 07:24:10 -0500

    Add d3 chart of emotions analysis (#68)
    * Commit for new branch
    
    * Add react-d3-basic
    
    * Add d3 chart of emotions analysis
    
    * Refactor emotions line graph

---
+++ src/components/LineGraph/index.js
@@ -0,0 +1,17 @@
+// import React from 'react';
+// import { LineChart } from 'react-d3-basic';
+//
+// export default (props) => {
+//   return (
+//     <LineChart
+//       title={props.title}
+//       data={props.data}
+//       width={props.width}
+//       height={props.height}
+//       margins={props.margins}
+//       chartSeries={props.chartSeries}
+//       x={props.x}
+//       xScale={props.xScale}
+//     />
+//   );
+// };
