3dec6ef7c91045c44e805ca137a9ab9386509e95
Andrew Vedady <drewvedady@gmail.com>
2016-05-05 09:50:53 -0400

    Update Social Data

--- src/containers/SocialDisplay/index.js
+++ src/containers/SocialDisplay/index.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { BarChart } from 'react-d3-basic';
 
+import { getTone } from '../../actions/index';
+import chartSetup from './ChartSetUp';
 /* component styles */
 // import { styles } from './styles.scss';
 
 export class SocialDisplay extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      socialData: [],
+      lastNerotic: 0,
+    };
   }
 
   render() {
@@ -16,6 +22,17 @@ export class SocialDisplay extends Component {
       <div className="row">
         <div className="col-lg-12 text-center">
           <h2>Neuroticism: {this.props.social.neuroticism_big5}</h2>
+          <BarChart
+            title={chartSetup.title}
+            data={this.state.socialData}
+            width={chartSetup.width}
+            height={chartSetup.height}
+            margins={chartSetup.margins}
+            chartSeries={chartSetup.chartSeries}
+            x={chartSetup.x}
+            xScale={chartSetup.xScale}
+            y={chartSetup.y}
+            yTicks={chartSetup.yTicks} />
         </div>
       </div>
     );
