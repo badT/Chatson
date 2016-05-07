536d3ef9c83c97fd1620cbb06b266c8111490de5
Ben Janes <brjanes@gmail.com>
2016-05-07 14:55:24 -0400

    Change formatting

--- src/components/DataDisplay/index.js
+++ src/components/DataDisplay/index.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-
 import DataContainer from '../../containers/DataContainer/DataContainer';
 
 /* component styles */
