67bd0078ab9e7f862db6e261b2119df98e782ae8
Dan Ramich <danold215@gmail.com>
2016-05-07 14:17:30 -0700

    Add additional API testing

--- app/routes.js
+++ app/routes.js
@@ -10,7 +10,6 @@ module.exports = (app, express) => {
   app.get('/messages', (req, res) => {
     messageController.getMessages((data) => {
       res.send(data);
-      res.end();
     });
   });
 
@@ -23,14 +22,16 @@ module.exports = (app, express) => {
 
     req.on('end', () => {
       const channel = data.toString();
-
-      // add the new channel to the list of connected channels, if it is not included already
-      establishConnection.connect(channel);
-      // send the channel name back to the client. client leaves current socket room and joins this one
-      res.send({ channel });
-      res.end();
+      // Checks to see if channel has a length then then creates a connection
+      if (channel !== '') {
+        // add the new channel to the list of connected channels, if it is not included already
+        establishConnection.connect(channel);
+        // send the channel name back to the client. client leaves current socket room and joins this one
+        res.send({ channel });
+      } else {
+        res.status(400).send('No channel data recieved');
+      }
     });
-
   });
 
   app.put('/api/watson/tone', (req, res) => {
@@ -41,16 +42,20 @@ module.exports = (app, express) => {
     });
 
     req.on('end', () => {
-      analyzer.runAnalysis(data)
-        .then((tone) => {
-          res.send(tone);
-          res.end();
-        })
-        .catch(err => {
-          console.error(err);
-          res.sendStatus(409);
-          res.end();
-        });
+      // Checks to see if data has a length then runs the analyzer
+      if (data.text !== '') {
+        analyzer.runAnalysis(data)
+          .then((tone) => {
+            res.send(tone);
+          })
+          .catch(err => {
+            res.sendStatus(500);
+          });
+      // sends back error message if data is empty
+      } else {
+        res.status(400).send('No message data recieved');
+      }
     });
   });
+
 };
