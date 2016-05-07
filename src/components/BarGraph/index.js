2e08e811f465f0339722c91f45a2d5e9b2cae80d
Andrew Vedady <drewvedady@gmail.com>
2016-05-07 17:03:39 -0400

    Change chart setup file

--- src/components/BarGraph/index.js
+++ src/components/BarGraph/index.js
@@ -1,19 +1,24 @@
-import React from 'react';
-import { BarChart } from 'react-d3-basic';
+// import React from 'react';
+// import { BarChart } from 'react-d3-basic';
 
-export default (props) => {
-  return (
-    <BarChart
-      title={props.title}
-      data={props.data}
-      width={props.width}
-      height={props.height}
-      margins={props.margins}
-      chartSeries={props.chartSeries}
-      x={props.x}
-      xScale={props.xScale}
-      y={props.y}
-      yTicks={props.yTicks} />
-  );
-};
-
+// export default (props) => {
+//   return (
+//     <BarChart
+//       title={props.title}
+//       data={props.data}
+//       width={props.width}
+//       height={props.height}
+//       margins={props.margins}
+//       chartSeries={props.chartSeries}
+//       x={props.x}
+//       xScale={props.xScale}
+//       y={props.y}
+//       yTicks={props.yTicks}
+//       showXGrid= {true}
+//       showYGrid= {true}
+//       showLegend={false}
+//       onMouseOver= {onMouseOver}
+//       onMouseOut= {onMouseOut}
+//     />
+//   );
+// };
