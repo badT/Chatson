c58b2f2318598170913603971b99459039e3daf4
Dan Ramich <danold215@gmail.com>
2016-05-07 15:47:25 -0700

    Add analyzer tests

--- app/test/analyzer.spec.js
+++ app/test/analyzer.spec.js
@@ -6,6 +6,27 @@ describe('analyzer test suite', () => {
   it('should expose a function', () => {
     expect(analyzer.runAnalysis).toBeA('function');
   });
+
+  it('should return tone analysis data', () => {
+    const message = { text: 'testing you watson' };
+
+    return analyzer.runAnalysis(message)
+    .then((data) => {
+      expect(data).toIncludeKey('toneData');
+    });
+  });
+
+  it('should return an error when no text is sent', () => {
+    const message = { text: '' };
+
+    return analyzer.runAnalysis(message)
+    .then((data) => {
+      expect(data.toneData).toNotExist();
+    }).catch((err) => {
+      expect(err).toExist();
+    });
+  });
+
   describe('calling runAnalysis', () => {
 
     beforeEach(() => {
@@ -16,10 +37,11 @@ describe('analyzer test suite', () => {
       expect.restoreSpies();
     });
 
-    it('is called', (done) => {
+    it('should be called', (done) => {
       analyzer.runAnalysis();
       expect(analyzer.runAnalysis).toHaveBeenCalled();
       done();
     });
+
   });
 });
