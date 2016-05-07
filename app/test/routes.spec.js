d15c423d8bda5a63d0ec66471b6840df6bbe5d3b
Dan Ramich <dramich@users.noreply.github.com>
2016-05-07 14:24:14 -0700

    Add additional API testing (#91)

--- app/test/routes.spec.js
+++ app/test/routes.spec.js
@@ -5,49 +5,63 @@ const supertest = require('supertest');
 const server = supertest.agent('http://localhost:3000');
 
 describe('routes test suite', () => {
-  // console.log(routes.app.post)
   it('should expose functions', () => {
     expect(routes).toBeA('function');
   });
 
-  it('should respond to POST requests', (done) => {
-    server
+  describe('POST request API handler', () => {
+    it('should respond to POST requests', (done) => {
+      server
+        .post('/api/channels/subscribe')
+        .send('nl_kripp')
+        .end((err, res) => {
+          expect(res.body).toEqual({ channel: 'nl_kripp' });
+          done();
+        });
+    });
+    // verify post request has data
+    it('should return an error for POST requests with no data', (done) => {
+      server
       .post('/api/channels/subscribe')
-      .send('nl_kripp')
+      .send('')
       .end((err, res) => {
-        expect(res.body).toEqual({ channel: 'nl_kripp' });
+        expect(res.status).toEqual(400);
         done();
       });
-  });
-
-  it('should respond to POST requests', (done) => {
-    server
-    .post('/api/channels/subscribe')
-    .send('nl_kripp')
-    .end((err, res) => {
-      expect(res.body).toEqual({ channel: 'nl_kripp' });
-      done();
     });
   });
 
-  it('should respond to PUT requests', (done) => {
-    server
-      .put('/api/watson/tone')
-      .send('testing for hate')
-      .end((err, res) => {
-        expect(res.body).toExist();
-        done();
-      });
-  });
+  describe('PUT request API handler', () => {
+    it('should respond to PUT requests', (done) => {
+      server
+        .put('/api/watson/tone')
+        .send('testing for hate')
+        .end((err, res) => {
+          expect(res.body).toExist();
+          done();
+        });
+    });
 
-  it('should return tone data from PUT requests', (done) => {
-    server
-      .put('/api/watson/tone')
-      .send('testing for hate')
-      .end((err, res) => {
-        expect(res.body.toneData).toExist();
-        done();
-      });
+    it('should return tone data from PUT requests', (done) => {
+      server
+        .put('/api/watson/tone')
+        .send('testing for hate')
+        .end((err, res) => {
+          expect(res.body.toneData).toExist();
+          done();
+        });
+    });
+
+    // verify post request has data
+    it('should return an error for PUT requests with no data', (done) => {
+      server
+        .put('/api/watson/tone')
+        .send('')
+        .end((err, res) => {
+          expect(res.status).toEqual(400);
+          done();
+        });
+    });
   });
 
 });
