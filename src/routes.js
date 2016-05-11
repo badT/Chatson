bd2ea150c6922e686b12c5f7a658b802bfafb3b7
Andrew Vedady <drewvedady@gmail.com>
2016-05-11 17:51:21 -0400

    Add route path for About page

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import { Route, IndexRoute } from 'react-router';
 /* containers */
 import { App } from 'components/App';
 import { Home } from 'components/Home';
+import { About } from 'components/About';
 import { DataDisplay } from 'components/DataDisplay';
 // import { List } from 'containers/List';
 
@@ -11,6 +12,7 @@ export default (
   <Route path="/" component={App}>
     <IndexRoute component={Home} />
     <Route path="/chat" component={DataDisplay} />
+    <Route path="/about" component={About} />
     <Route status={404} path="*" component={Home} />
   </Route>
 );
