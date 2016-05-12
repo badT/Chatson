86a1fccff5959639318bc5837b63e49eeea7416f
Andrew Vedady <drewvedady@gmail.com>
2016-05-12 15:18:22 -0400

    Update paragraph to new name

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -11,9 +11,9 @@ export class About extends Component {
           <div className="row">
             <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
             <p>
-              twitchBot was created to give a user a visual representation of what is going on in a Twitch channel - the world’s leading social video platform and community for gamers. With our mood charts and graphs analyzing the realtime flow of comments, a user can instantly decide whether to join the channel. Chat messages are send to the Watson cognitive system API to return an analysis of emotional & social tone that is streamed to connected users.
+              Chatson was created to give a user a visual representation of what is going on in a Twitch channel - the world’s leading social video platform and community for gamers. With our mood charts and graphs analyzing the realtime flow of comments, a user can instantly decide whether to join the channel. Chat messages are send to the Watson cognitive system API to return an analysis of emotional & social tone that is streamed to connected users.
               
-              Users select one of the top 25 currently active twitch streams and are presented with real­time visuals of the mood, frequency and length of incoming messages. Users may also view long term data from 4 channels selected by our team.
+              Users select one of the top 25 currently active streams and are presented with real­time visuals of the mood, frequency and length of incoming messages. Users may also view long term data from 4 channels selected by our team.
             </p>
             </div>
           </div>
