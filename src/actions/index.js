2395142f90a66a32da15e43dde6c33c0b3bf1b58
Ben Janes <brjanes@gmail.com>
2016-05-12 19:32:32 -0400

    Reduce the displayed channels to top 12

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // get top twitch channels
 export const GET_CHANNELS = 'GET_CHANNELS';
 export function getChannels() {
-  const request = axios.get('https://api.twitch.tv/kraken/streams?api_version=3&limit=25');
+  const request = axios.get('https://api.twitch.tv/kraken/streams?api_version=3&limit=12');
   return {
     type: GET_CHANNELS,
     payload: request,
