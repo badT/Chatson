a5a1c64a05a7f3e3e736da6e19328b35fe53d80f
Ben Janes <brjanes@gmail.com>
2016-05-12 15:40:54 -0400

    Refactor to break line graph into component parts

---
+++ src/components/LineGraph/LineGraphKey/index.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { emoColors, socColors, splotchDesc, splotchDescLT25, splotchDescLT50, splotchDescGT75 } from '../descriptions';
+import { capitalizeFirstLetter } from '../helpers';
+/* component styles */
+import { styles } from './styles.scss';
+
+export default class LineGraphKey extends Component {
+  renderSplotches(colorKey, key) {
+    if (!colorKey) return;
+    const splotches = [];
+    for (let splotch in colorKey) {
+      splotches.push(
+        <div key={splotch} className="block-grid-item splotch-holder">
+          <div className="color-splotch-holder">
+            <span id={`${splotch}-splotch`} className="color-splotch"></span>
+            <span className="splotch-tooltip tooltip">
+              {`Avg for last 90 seconds: ${this.props[key][splotch].toFixed(2)}`}<br/>
+              {`${this.props[key][splotch].toFixed(2) <= 0.25 ? splotchDescLT25[splotch] : ''}`}
+              {`${this.props[key][splotch].toFixed(2) <= 0.5 ? splotchDescLT50[splotch] : ''}`}
+              {`${this.props[key][splotch].toFixed(2) >= 0.75 ? splotchDescGT75[splotch] : ''}`}
+            </span>
+          </div>
+          <span className="splotch-label">
+            <span className={`${splotch}-splotch-text`}>{capitalizeFirstLetter(splotch)}</span>
+            <span className="splotch-label-tooltip tooltip">
+              {splotchDesc[splotch]}
+            </span>
+          </span>
+        </div>
+      );
+    }
+    return (
+      <div className="block-grid-md-5 block-grid-sm-3 block-grid-xs-2 splotches-grid">
+        {splotches}
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div className={`${styles}`}>
+        <section className={`graph-key dom-emo-${this.props.dominantEmo} dom-soc-${this.props.dominantSoc}`}>
+          <div className={`row splotch-row ${this.props.activeGraph === 'emotion' ? 'splotch-row-active' : ''}`}>
+            {this.renderSplotches(emoColors, 'emotionKey')}
+          </div>
+          <div className={`row splotch-row ${this.props.activeGraph === 'social' ? 'splotch-row-active' : ''}`}>
+            {this.renderSplotches(socColors, 'socialKey')}
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
---
+++ src/components/LineGraph/LineGraphTabs/index.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+/* component styles */
+import { styles } from './styles.scss';
+
+export default class LineGraphTabs extends Component {
+  render() {
+    return (
+      <div className={`${styles}`}>
+        <section className="row">
+          <div className="col-xs-12 graph-tab-holder">
+            <h2
+              className={`graph-tab ${this.props.activeGraph === 'emotion' ? 'tab-active' : ''}`}
+              onClick={() => this.props.toggleGraph('emotion')}>
+                Channel Emotions
+            </h2>
+            <h2
+              className={`graph-tab ${this.props.activeGraph === 'social' ? 'tab-active' : ''}`}
+              onClick={() => this.props.toggleGraph('social')}>
+                Social Attributes
+            </h2>
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
