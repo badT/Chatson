e5bada62f6eff669ca7ce33b4dfa1a6563619d6b
Ben Janes <brjanes@gmail.com>
2016-05-13 19:51:06 -0400

    Add alt tags for footer logos

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -11,17 +11,17 @@ export function Footer() {
         <div className="row">
           <div className="col-xs-4">
             <a href="http://www.ibm.com/smarterplanet/us/en/ibmwatson/developercloud/tone-analyzer.html" target="_blank">
-              <img className="footer-icon" src={taIcon} />
+              <img className="footer-icon" src={taIcon} alt="Watson Tone Analyzer Logo" />
             </a>
           </div>
           <div className="col-xs-4">
             <a href="https://www.twitch.tv/" target="_blank">
-              <img className="footer-icon" src={twitchIcon} />
+              <img className="footer-icon" src={twitchIcon} alt="Twitch Logo" />
             </a>
           </div>
           <div className="col-xs-4">
             <a href="https://github.com/badT/twitchBot" target="_blank">
-              <img className="footer-icon" src={ghIcon} />
+              <img className="footer-icon" src={ghIcon} alt="GitHub Logo" />
             </a>
           </div>
         </div>
