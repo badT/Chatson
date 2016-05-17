3fd6ffe2fdd5497512123f27d39b2f0d5bda89ec
Andrew Vedady <drewvedady@gmail.com>
2016-05-16 20:25:28 -0400

    Add long term data bar graphs (#158)

--- src/reducers/reducerLongTermTone.js
+++ src/reducers/reducerLongTermTone.js
@@ -4,7 +4,7 @@ const INITIAL_STATE = { longTermTone: null };
 export default function (state = INITIAL_STATE, action) {
   switch (action.type) {
     case GET_LONGTERMTONE:
-      return { ...state, longTermTone: action.payload.data.longTermToneData };
+      return { ...state, longTermTone: action.payload.data };
 
     default:
       return state;
