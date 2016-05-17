55db1154c3ab86a021fa218ec19b2ce2b4502a81
Terry Capan <tcapanjr@gmail.com>
2016-05-17 18:47:17 -0500

    Add tests for new action creators and reducers

--- src/test/actionsSpec/actions.spec.js
+++ src/test/actionsSpec/actions.spec.js
@@ -38,11 +38,35 @@ describe('action creators', () => {
       });
   });
 
+  it('creates UNSET_CHANNEL action when unsetChannel is called', () => {
+    const expectedAction = {
+      type: actions.UNSET_CHANNEL,
+      payload: null,
+    };
+    expect(actions.unsetChannel()).toEqual(expectedAction);
+  });
+
   it('creates GET_TONE action when getTone is called', () => {
     return store.dispatch(actions.getTone())
       .then(() => {
         const createdActions = store.getActions();
         expect(createdActions[0].type).toEqual(actions.GET_TONE);
+      });
+  });
+
+  it('creates UNSET_TONE action when unsetTone is called', () => {
+    const expectedAction = {
+      type: actions.UNSET_TONE,
+      payload: null,
+    };
+    expect(actions.unsetTone()).toEqual(expectedAction);
+  });
+
+  it('creates GET_LONGTERMTONE action when getLongTermTone is called', () => {
+    return store.dispatch(actions.getLongTermTone())
+      .then(() => {
+        const createdActions = store.getActions();
+        expect(createdActions[0].type).toEqual(actions.GET_LONGTERMTONE);
       });
   });
 });
