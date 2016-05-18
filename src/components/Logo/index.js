7072b8bb8dd036416f86d9ef6c63e5c905bf1114
Terry Capan <tcapanjr@gmail.com>
2016-05-18 16:00:14 -0500

    Add reverse mouseover to logo animation

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -6,24 +6,30 @@ import { styles } from './styles.scss';
 
 export default class Logo extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      logo: new TimelineMax,
+      hatson: new TimelineMax,
+      fullLogo: false,
+      animating: false,
+    };
+  }
+
   componentDidMount() {
-    this.logoAnimate();
+    setTimeout(() => { this.toggleAnimation(); }, 2000);
   }
 
   logoAnimate() {
-    const logo = new TimelineMax;
-    const hatson = new TimelineMax;
 
-    logo.to('#mouth', 2, { delay: 2, rotation: 270, x: 5, y: -4, transformOrigin: '10px 5px' })
-        .to('#left_eye', 2, { rotation: 90, x: 40.9, y: -6 }, '-=2')
-        .to('#right_eye', 2, { rotation: 90, x: 18, y: 19 }, '-=2')
-        .to('#left_ball', 2, { rotation: 90, x: 37, y: -4 }, '-=2')
-        .to('#right_ball', 2, { rotation: 90, x: 14, y: 20 }, '-=2')
-        .to('#left_ball', 0.75, { opacity: 0, x: 45, y: -20 }, '-=1.15')
-        .to('#right_ball', 0.75, { opacity: 0, x: 20, y: 40 }, '-=0.8')
-        .duration(4);
+    const forward = () => {
+      this.state.logo.to('#mouth', 2, { rotation: 270, x: 5, y: -4, transformOrigin: '10px 5px' })
+        .to('.eyes', 2, { rotation: 90, x: 41, y: -4 }, '-=2')
+        .to('#left_ball', 0.75, { opacity: 0, x: 5, y: -45 }, '-=1.15')
+        .to('#right_ball', 0.75, { opacity: 0, x: 25, y: -35 }, '-=0.8')
+        .duration(2);
 
-    hatson.to('#hhh', 0, { scale: 0.25 })
+      this.state.hatson.to('#hhh', 0, { scale: 0.25 })
         .to('#aaa', 0, { scale: 0.25 })
         .to('#ttt', 0, { scale: 0.25 })
         .to('#sss', 0, { scale: 0.25 })
@@ -35,24 +41,60 @@ export default class Logo extends Component {
         .to('#sss', 0.75, { opacity: 100, scale: 1.7, x: 32, y: 3 }, '-=0.5')
         .to('#ooo', 0.75, { opacity: 100, scale: 1.7, x: 42, y: 3 }, '-=0.5')
         .to('#nnn', 0.75, { opacity: 100, scale: 1.7, x: 54, y: 3 }, '-=0.5')
-        .duration(4);
+        .duration(2);
+    };
+
+    const reverse = () => {
+      this.state.logo.reverse(0);
+      this.state.hatson.reverse(0);
+      this.setState({
+        logo: new TimelineMax,
+        hatson: new TimelineMax,
+      });
+    };
+
+    return {
+      forward,
+      reverse,
+    };
+  }
+
+  toggleAnimation() {
+    if (this.state.animating) return;
+    this.setState({ animating: true });
+
+    if (this.state.fullLogo) {
+      this.logoAnimate().reverse();
+      this.setState({ fullLogo: false });
+      setTimeout(() => {
+        this.setState({ animating: false });
+      }, 2000);
+    } else {
+      this.setState({ fullLogo: true });
+      this.logoAnimate().forward();
+      setTimeout(() => {
+        this.setState({ animating: false });
+      }, 2000);
+    }
   }
 
   render() {
     return (
-      <div className={`${styles}`}>
+      <div className={`${styles}`} onMouseEnter={() => { this.toggleAnimation(); }}>
         <span className="logo-holder">
           <svg className="logo" width="60" height="60" viewBox="-2 -2 64 74">
-            <line id="left_eye" fill="none" stroke="#ffffff" strokeWidth="3" stroke-miterlimit="10" x1="12.5" y1="8.9" x2="24.3" y2="8.9" />
-            <line id="right_eye" fill="none" stroke="#ffffff" strokeWidth="3" stroke-miterlimit="10" x1="35.8" y1="8.9" x2="47.7" y2="8.9" />
-            <path
-              id="left_ball" fill="#FF1D25" stroke="#FF1D25" strokeWidth="1.35" stroke-miterlimit="10" d="M14.9,10.3c0,1.9,1.4,3.5,3,3.8
-              c1.9,0.3,4.1-1.4,4.1-3.8"
-            />
-            <path
-              id="right_ball" fill="#FF1D25" stroke="#FF1D25" strokeWidth="1.35" stroke-miterlimit="10" d="M38.1,10.3c0,1.9,1.4,3.5,3,3.8
-             c1.9,0.3,4.1-1.4,4.1-3.8"
-            />
+            <g className="eyes">
+              <line id="left_eye" fill="none" stroke="#ffffff" strokeWidth="3" stroke-miterlimit="10" x1="12.5" y1="8.9" x2="24.3" y2="8.9" />
+              <line id="right_eye" fill="none" stroke="#ffffff" strokeWidth="3" stroke-miterlimit="10" x1="35.8" y1="8.9" x2="47.7" y2="8.9" />
+              <path
+                id="left_ball" fill="#FF1D25" stroke="#FF1D25" strokeWidth="1.35" stroke-miterlimit="10" d="M14.9,10.3c0,1.9,1.4,3.5,3,3.8
+                c1.9,0.3,4.1-1.4,4.1-3.8"
+              />
+              <path
+                id="right_ball" fill="#FF1D25" stroke="#FF1D25" strokeWidth="1.35" stroke-miterlimit="10" d="M38.1,10.3c0,1.9,1.4,3.5,3,3.8
+                c1.9,0.3,4.1-1.4,4.1-3.8"
+              />
+            </g>
             <path
               id="mouth" fill="none" stroke="#ffffff" strokeWidth="5" stroke-miterlimit="10" d="M46.1,47.6c0.3-1.3,1.2-5.4-0.7-9.9
               c-1.7-4.2-4.9-6.4-5.8-7.1c-0.6-0.4-1.7-1.2-3.6-1.9c-0.7-0.3-3.6-1.3-7.4-1c-1.3,0.1-4.6,0.4-8,2.5c-1,0.6-3.2,2-4.9,4.6
